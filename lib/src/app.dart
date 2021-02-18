5b8080dc507f59c680c8f3a2062d3f27a62f5529
SomeoneAndNoone <hamidovhamid1998@gmail.com>
2021-02-18 20:05:38 +0000

    network provider and repository layers created

---
+++ lib/src/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LoginBlocProvider(
+      child: GoalsBlocProvider(
+        child: MaterialApp(
+          theme: ThemeData(accentColor: Colors.black, primaryColor: Colors.amber),
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Goals",
+                style: TextStyle(color: Colors.black),
+              ),
+              backgroundColor: Colors.amber,
+              elevation: 0.0,
+            ),
+            body: LoginScreen(),
+          ),
+        ),
+      ),
+    );
+  }
+}
